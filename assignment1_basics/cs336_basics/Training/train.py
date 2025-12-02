d2762282c8bca40eb2eea155d1d08c81afa27a80
dingzigeng <1605383434@qq.com>
2025-12-02 14:20:11 +0800

    fix: fix some error in training loop

--- assignment1_basics/cs336_basics/Training/train.py
+++ assignment1_basics/cs336_basics/Training/train.py
@@ -8,7 +8,8 @@ from lr_cosine_schedule import lr_cosine_schedule
 from cross_entropy_loss import cross_entropy
 from check_point import save_checkpoint, load_checkpoint
 
-from ..Model.TransformerLM import TransformerLM
+from cs336_basics.Model.TransformerLM import TransformerLM
+
 
 # def train(model, optimizer, train_data, val_data, paths):
 
@@ -21,50 +22,10 @@ from loguru import logger
 if __name__ == "__main__":
     logger.add("./data/log/train_v0.log", rotation="1 day", retention="7 days", level="INFO")
 
-    # # 设置所有超参数
-    # # 模型参数
-    # model_config = {
-    #     "vocab_size": 10000,      # 词汇表大小
-    #     "context_length": 256,    # 上下文长度
-    #     "num_layers": 4,          # Transformer Block数
-    #     "num_heads": 16,          # 注意力头数
-    #     "d_model": 512,           # 嵌入空间维度
-    #     "d_ff": 1344,             # 前馈网络维度
-    #     "rope_theta": 10000,      # RoPE参数
-    # }
-    
-    # # 优化器参数
-    # optim_config = {
-    #     "lr": 3e-4,               # 学习率
-    #     "weight_decay": 1e-2,     # 权重衰减
-    #     "betas": (0.9, 0.999),    # AdamW的beta参数
-    #     "max_norm": 1.0,          # 梯度裁剪的最大范数
-    # }
-    
-    # # 训练参数
-    # train_config = {
-    #     "batch_size": 16,         # 批次大小
-    #     "total_epochs": 0.5,      # 训练轮数
-    #     "checkpoint_freq": 2000,  # 每隔多少步保存一次检查点
-    #     "log_freq": 10,           # 每隔多少步记录一次日志
-    #     "val_freq": 400,          # 每隔多少步在验证集上评估
-    #     "val_batch_size": 16,     # 验证时的批次大小
-    #     "val_batches": 20,        # 验证时使用的批次数量
-    # }
-    
-    # # 数据路径
-    # data_paths = {
-    #     "training_dataset_path": "./data/token/TinyStories_train_10000_token_ids.npy",
-    #     "validation_dataset_path": "./data/token/TinyStories_valid_10000_token_ids.npy",  # 验证集路径
-    #     "checkpoint_load_path": None,  # 模型检查点路径
-    #     "checkpoint_save_format": "./data/model/checkpoint_v0_{}.pt",  # 检查点保存路径格式
-    #     "final_model_path": "./data/model/final_model_v0.pt",  # 最终模型保存路径
-    # }
-    
     # 初始化wandb
     run = wandb.init(
         project="cs336-assignment-1",
-        name="train_v1",
+        name="train_batch128_lr3e-3",
         config={
             "model": config.model,
             "optimizer": config.optimizer,
@@ -123,8 +84,8 @@ if __name__ == "__main__":
 
     # 计算训练所需step
     total_tokens = training_dataset.shape[0]
-    total_steps = int(config.train["total_epochs"] * total_tokens) // (config.train["batch_size"] * config.train["context_length"])
-    logger.info(f"总token数: {total_tokens}, 训练轮数: {config.train['total_epochs']}, batch大小: {config.train['batch_size']}, 上下文长度: {config.train['context_length']}")
+    total_steps = int(config.train["total_epochs"] * total_tokens) // (config.train["batch_size"] * config.model["context_length"])
+    logger.info(f"总token数: {total_tokens}, 训练轮数: {config.train['total_epochs']}, batch大小: {config.train['batch_size']}, 上下文长度: {config.model['context_length']}")
     logger.info(f"总训练步数: {total_steps}")
 
     # step循环开始
@@ -134,11 +95,11 @@ if __name__ == "__main__":
         optimizer.zero_grad()
 
         # 使用余弦退火更新学习率
-        it=step,
-        max_learning_rate=config.optimizer["lr"],
-        min_learning_rate=config.optimizer["lr"] * 0.01,
-        warmup_iters=int(0.05 * total_steps),
-        cosine_cycle_iters=total_steps,
+        it=step
+        max_learning_rate=config.optimizer["lr"]
+        min_learning_rate=config.optimizer["lr"] * 0.01
+        warmup_iters=int(0.05 * total_steps)
+        cosine_cycle_iters=total_steps
         lr_now = lr_cosine_schedule(
             t = it,
             a_max=max_learning_rate,
@@ -153,7 +114,7 @@ if __name__ == "__main__":
         inputs, targets = get_batch(
             training_dataset,
             batch_size=config.train["batch_size"],
-            context_length=config.train["context_length"],
+            context_length=config.model["context_length"],
             device="cuda:0" if device.type == "cuda" else "cpu"
         )
 
@@ -170,7 +131,7 @@ if __name__ == "__main__":
         if step % config.train["log_freq"] == 0:
             grad_norm = compute_grad_norm(model.parameters())
         
-        gradient_clip(model.parameters(), max_l2_norm=config.train["max_norm"]) # 梯度裁剪
+        gradient_clip(model.parameters(), max_l2_norm=config.optimizer["max_norm"]) # 梯度裁剪
         optimizer.step()
 
         # 日志记录
@@ -188,7 +149,7 @@ if __name__ == "__main__":
                 dataset=validation_dataset,
                 device=device,
                 batch_size=config.train["val_batch_size"],
-                context_length=config.train["context_length"],
+                context_length=config.model["context_length"],
                 num_batches=config.train["val_batches"]
             )
             logger.info(f"验证集损失: {val_loss}")
